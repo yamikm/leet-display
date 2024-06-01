dac7b7677574f43f7bbece569b115d55071807b2
yamikm <yamik.mangukiya@gmail.com>
2024-06-01 13:29:50 -0700

    updating links

--- src/links.js
+++ src/links.js
@@ -1,6 +1,6 @@
 const links = {
-    "hello" : 'https://raw.githubusercontent.com/abhadani/FoodOrderApp/main/src/App.js',
-    "hello1" : 'https://raw.githubusercontent.com/abhadani/FoodOrderApp/main/src/components/Layout/Header.js',
+    "1. Two Sum" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/1_two_sum.cpp',
+    "2. Add Two Numbers LL" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/2_Add_two_numbers_linked_list.cpp',
     "hello2" : 'https://raw.githubusercontent.com/abhadani/FoodOrderApp/main/src/index.css',
     "hello3" : 'https://raw.githubusercontent.com/pritamzope/OS/master/GUI/box/kernel.c',
 
