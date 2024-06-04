c5da0460fae7f47b3e210029f08128ff5d9d8625
yamikm <yamik.mangukiya@gmail.com>
2024-06-03 22:56:04 -0700

    Adding 77 46 typo

--- src/links.js
+++ src/links.js
@@ -1,7 +1,7 @@
 const links = {
     "001. Two Sum" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/1_two_sum.cpp',
     "002. Add Two Numbers LL" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/2_Add_two_numbers_linked_list.cpp',
-    "077. Combination Sum" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/077_Combniation.cpp',
+    "077. Combination Sum" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/077_Combination.cpp',
     "046. Permutation" : 'https://raw.githubusercontent.com/yamikm/leet-display/main/src/algorithms/046_Permutation.cpp',
 
 };
